e54518c4872f1f41c57fe8d2f728336e51a0e390
nurdaulettn <nurdaulettolegen26@gmail.com>
2026-06-10 20:26:35 +0500

    Add a radio to selecting role

--- src/main/java/kz/nurdaulet/controller/AuthController.java
+++ src/main/java/kz/nurdaulet/controller/AuthController.java
@@ -2,6 +2,7 @@ package kz.nurdaulet.controller;
 
 import jakarta.validation.Valid;
 import kz.nurdaulet.dto.UserCreateDto;
+import kz.nurdaulet.entity.enums.Role;
 import kz.nurdaulet.exception.UserCreatingException;
 import kz.nurdaulet.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AuthController {
@@ -42,7 +45,12 @@ public class AuthController {
         }
 
         try {
-            user.setRole("CUSTOMER");
+            if (Role.MANAGER.name().equals(user.getRole())) {
+                user.setRole(Role.MANAGER.name());
+            } else {
+                user.setRole("CUSTOMER");
+            }
+
             userService.create(user);
 
             return "redirect:/login?registered=true";
