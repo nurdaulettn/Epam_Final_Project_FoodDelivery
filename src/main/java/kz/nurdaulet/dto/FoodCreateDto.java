9d09354e314bd2f82c755f5f125168810945cf99
nurdaulettn <nurdaulettolegen26@gmail.com>
2026-06-20 19:22:23 +0500

    add food create dto

---
+++ src/main/java/kz/nurdaulet/dto/FoodCreateDto.java
@@ -0,0 +1,50 @@
+package kz.nurdaulet.dto;
+
+public class FoodCreateDto {
+    private String name;
+    private String description;
+    private Double price;
+    private Long categoryId;
+
+    public FoodCreateDto() {
+    }
+
+    public FoodCreateDto(String name, String description, Double price, Long categoryId) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
