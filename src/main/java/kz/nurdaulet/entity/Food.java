c3b599345d05b9a3d7a57b9ba27c045af06cb198
nurdaulettn <nurdaulettolegen26@gmail.com>
2026-06-20 19:20:31 +0500

    add food entity

---
+++ src/main/java/kz/nurdaulet/entity/Food.java
@@ -0,0 +1,82 @@
+package kz.nurdaulet.entity;
+
+public class Food {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean isAvailable;
+    private Long restaurant_id;
+    private Long category_id;
+
+    public Food() {
+    }
+
+    public Food(Long id, String name, String description,
+                Double price, Boolean isAvailable,
+                Long restaurant_id, Long category_id) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.isAvailable = isAvailable;
+        this.restaurant_id = restaurant_id;
+        this.category_id = category_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+
+    public Long getRestaurant_id() {
+        return restaurant_id;
+    }
+
+    public void setRestaurant_id(Long restaurant_id) {
+        this.restaurant_id = restaurant_id;
+    }
+
+    public Long getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(Long category_id) {
+        this.category_id = category_id;
+    }
+}
